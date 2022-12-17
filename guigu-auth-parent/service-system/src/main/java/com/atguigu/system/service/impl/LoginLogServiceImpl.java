704477765b12cb9d0fa37fd525c50369fbcb324f
lwq <2065277594@qq.com>
2022-12-17 15:43:08 +0800

    1

--- guigu-auth-parent/service-system/src/main/java/com/atguigu/system/service/impl/LoginLogServiceImpl.java
+++ guigu-auth-parent/service-system/src/main/java/com/atguigu/system/service/impl/LoginLogServiceImpl.java
@@ -17,6 +17,9 @@ public class LoginLogServiceImpl implements LoginLogService {
     @Autowired
     private LoginLogMapper loginLogMapper;
 
+    /**
+     * 记录登录日志
+     */
     @Override
     public void recordLoginLog(String username, Integer status,
                                String ipaddr, String message) {
@@ -28,25 +31,27 @@ public class LoginLogServiceImpl implements LoginLogService {
         loginLogMapper.insert(sysLoginLog);
     }
 
-    //条件分页查询登录日志
+    /**
+     * 查询登录日志
+     */
     @Override
     public IPage<SysLoginLog> selectPage(long page, long limit, SysLoginLogQueryVo sysLoginLogQueryVo) {
         //创建page对象
-        Page<SysLoginLog> pageParam = new Page(page,limit);
+        Page<SysLoginLog> pageParam = new Page(page, limit);
         //获取条件值
         String username = sysLoginLogQueryVo.getUsername();
         String createTimeBegin = sysLoginLogQueryVo.getCreateTimeBegin();
         String createTimeEnd = sysLoginLogQueryVo.getCreateTimeEnd();
         //封装条件
         QueryWrapper<SysLoginLog> wrapper = new QueryWrapper<>();
-        if(!StringUtils.isEmpty(username)) {
-            wrapper.like("username",username);
+        if (!StringUtils.isEmpty(username)) {
+            wrapper.like("username", username);
         }
-        if(!StringUtils.isEmpty(createTimeBegin)) {
-            wrapper.ge("create_time",createTimeBegin);
+        if (!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge("create_time", createTimeBegin);
         }
-        if(!StringUtils.isEmpty(createTimeBegin)) {
-            wrapper.le("create_time",createTimeEnd);
+        if (!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.le("create_time", createTimeEnd);
         }
         //调用mapper方法
         IPage<SysLoginLog> pageModel = loginLogMapper.selectPage(pageParam, wrapper);
--- guigu-auth-parent/service-system/src/main/java/com/atguigu/system/service/impl/OperLogServiceImpl.java
+++ guigu-auth-parent/service-system/src/main/java/com/atguigu/system/service/impl/OperLogServiceImpl.java
@@ -17,15 +17,20 @@ public class OperLogServiceImpl implements OperLogService {
     @Autowired
     private OperLogMapper operLogMapper;
 
+    /**
+     * 记录操作日志
+     */
     @Override
     public void saveSysLog(SysOperLog sysOperLog) {
         operLogMapper.insert(sysOperLog);
     }
 
-    //操作日志分页查询
+    /**
+     * 查询操作日志
+     */
     @Override
     public IPage<SysOperLog> selectPage(Long page, Long limit, SysOperLogQueryVo sysOperLogQueryVo) {
-        Page<SysOperLog> pageParam = new Page<>(page,limit);
+        Page<SysOperLog> pageParam = new Page<>(page, limit);
         //获取条件值
         String title = sysOperLogQueryVo.getTitle();
         String operName = sysOperLogQueryVo.getOperName();
@@ -33,17 +38,17 @@ public class OperLogServiceImpl implements OperLogService {
         String createTimeEnd = sysOperLogQueryVo.getCreateTimeEnd();
         //封装参数
         QueryWrapper<SysOperLog> wrapper = new QueryWrapper<>();
-        if(!StringUtils.isEmpty(title)) {
-            wrapper.like("title",title);
+        if (!StringUtils.isEmpty(title)) {
+            wrapper.like("title", title);
         }
-        if(!StringUtils.isEmpty(operName)) {
-            wrapper.like("oper_name",operName);
+        if (!StringUtils.isEmpty(operName)) {
+            wrapper.like("oper_name", operName);
         }
-        if(!StringUtils.isEmpty(createTimeBegin)) {
-            wrapper.ge("create_time",createTimeBegin);
+        if (!StringUtils.isEmpty(createTimeBegin)) {
+            wrapper.ge("create_time", createTimeBegin);
         }
-        if(!StringUtils.isEmpty(createTimeEnd)) {
-            wrapper.le("create_time",createTimeEnd);
+        if (!StringUtils.isEmpty(createTimeEnd)) {
+            wrapper.le("create_time", createTimeEnd);
         }
         //调用mapper方法实现分页条件查询
         IPage<SysOperLog> sysOperLogPage = operLogMapper.selectPage(pageParam, wrapper);
